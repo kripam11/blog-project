a79eeb7bb3ff03712886f0dd288d4173c0c855fe
kripamiddha <kripamiddha46@gmail.com>
2026-06-20 14:08:03 +0530

    Fix Vercel deployment: export Express app for serverless

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 import usersRouter from "./routes/users.js";
 import postsRouter from "./routes/posts.js";
 import commentsRouter from "./routes/comments.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(usersRouter);
 app.use(postsRouter);
 app.use(commentsRouter);
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+if (!process.env.VERCEL) {
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
+
+export default app;
